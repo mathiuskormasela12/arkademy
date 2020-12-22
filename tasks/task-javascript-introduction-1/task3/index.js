fd0e19e104cfe9a4f2ae1f4c8cb2188429a98718
mathiuskormasela12 <mathiuskormasela12@gmail.com>
2020-12-22 12:21:52 +0700

    Secondary Update

--- tasks/task-javascript-introduction-1/task3/index.js
+++ tasks/task-javascript-introduction-1/task3/index.js
@@ -1,15 +1,34 @@
-const printSegitiga = 5;
+// const printSegitiga = 5;
 
-if(typeof printSegitiga === 'number') {
-  for(let vertical = printSegitiga; vertical >= 1; vertical--) {
-    let str = '';
+// if(typeof printSegitiga === 'number') {
+//   for(let vertical = printSegitiga; vertical >= 1; vertical--) {
+//     let str = '';
     
-    for(let horizontal = 1; horizontal <= vertical; horizontal++) {
-      str += `${horizontal} `;
+//     for(let horizontal = 1; horizontal <= vertical; horizontal++) {
+//       str += `${horizontal} `;
+//     }
+
+//     console.log(str);
+//   }
+// } else {
+//   console.log('Data harus number');
+// }
+
+function printSegitiga(num) {
+  if(typeof num === 'number') {
+    let str = '';
+
+    for(let vertical = num; vertical >= 1; vertical--) {
+      for(let horizontal = 1; horizontal <= vertical; horizontal++) {
+        str += `${horizontal} `;
+      }
+      str += '\n'
     }
 
-    console.log(str);
+    return str;
+  } else {
+    return 'Data harus number';
   }
-} else {
-  console.log('Data harus number');
 }
+
+console.log(printSegitiga(5));
