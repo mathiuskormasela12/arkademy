a60ab72f9d58fa8e23c653bd3ef8d68e0524477f
mathiuskormasela12 <mathiuskormasela12@gmail.com>
2020-12-29 12:41:48 +0700

    Abstraction

--- tasks/javascript/day-5/task3/index.js
+++ tasks/javascript/day-5/task3/index.js
@@ -1,22 +1,51 @@
 function sorting(numArr) {
   return numArr.map(item => {
+    /* 
+      5. Pecah kembali setiap item dari string 
+      num yg ada pada variable numArr
+    */
     return item.split('')
+      // 6. Urutkan element-element yg sudah di pecah itu 
       .sort((min, max) => min - max)
   });
 }
 
-function divideAndSort(num) {
-  let numStr = num.toString();
-  let numArr = numStr.split('0');
-  
-  numArr = sorting(numArr);
-
-  let numAsc = numArr.map(item => item.join(''));
-  numAsc = numAsc.join('');
-
-  numAsc = parseInt(numAsc);
-  
-  console.log(numAsc)
+function joinAllArray(numArr) {
+  return numArr
+    // 7. Gabungkan setiap item yang sudah di urutkan
+    .map(item => item.join(''))
+    // 8. Gabungkan setiap item yang sudah di pecah
+    .join('');
 }
 
-divideAndSort(5956560159466056)
+function divideAndSort(num) {
+
+  /*
+    1. inisialisasi variable
+    num, numArr, numAsc, numStr
+
+
+    3. Konversi parameter num menjadi string 
+    dan masukan kedalam variable numStr
+  */
+  let numStr = num.toString();
+  /* 
+    4. Pecah string num menjadi array 
+    dengan separator angka 0 dan masukan
+    ke dalam variable numArr
+  */
+  let numArr = sorting(numStr.split('0'));
+
+  /* 
+     9. Masukan kedalam variable 
+     numAsc hasil dari penggabungan 
+     arraynya
+  */
+  let numAsc = joinAllArray(numArr);
+  // 10. Konversi variable numAsc menjadi number 
+  return parseInt(numAsc);
+}
+
+// 2. Input number kedalam variable num 
+// 11. cetak hasilnya
+console.log(divideAndSort(5956560159466056));
